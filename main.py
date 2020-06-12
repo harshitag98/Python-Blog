50f3dc4d1fa1d4230b2602910ab135d21dc9e421
harshitag98 <harshitagrawal920@gmail.com>
2020-06-12 16:38:18 +0530

    added main python file with static directory

---
+++ main.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail
+import json
+
+with open('config.json', 'r') as con:
+    params = json.load(con)['params']
+
+app = Flask(__name__)
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['mail_user'],
+    MAIL_PASSWORD=params['mail_pass']
+)
+mail = Mail(app)
+
+if(params['local_server']==True):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_num = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
+
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    slug = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
+
+@app.route("/")
+def home():
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', params=params)
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if(request.method=="POST"):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        msg = request.form.get('message')
+        entry = Contacts(name=name, email=email, phone_num=phone, message=msg, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+        mail.send_message("New message from "+name ,
+                          sender = email,
+                          recipients = [params['mail_user']],
+                          body = msg + "\n" + phone
+                          )
+    return render_template('contact.html', params=params)
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+app.run(debug=True)
