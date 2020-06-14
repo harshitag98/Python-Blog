8d5b39693d9869e0f267bb9ea0c20e3d8599b6fe
harshitag98 <harshitagrawal920@gmail.com>
2020-06-14 17:15:56 +0530

    pagenation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_mail import Mail
+import math
 import json
 
 with open('config.json', 'r') as con:
@@ -44,8 +45,23 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/params['no_of_posts'])
+    page = request.args.get('page')
+    if(not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*params['no_of_posts'] : (page-1)*params['no_of_posts']+params['no_of_posts']]
+    if(page==1):
+        prev = "#"
+        next = "/?page="+str(page+1)
+    elif(page==last):
+        prev = "/?page="+str(page-1)
+        next = "#"
+    else:
+        prev = "/?page="+str(page-1)
+        next = "/?page="+str(page+1)
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 @app.route("/about")
 def about():
